59e7edda11ed9c70ea7b7158bd64381024c12144
Vas-Laptop <PDFOrigami@gmail.com>
2020-03-11 15:58:33 +0200

    Updates

---
+++ precache-manifest.a7d8968f4874914fd5d485555b5feb8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d3570df9a202ab01d2ef208b08d4f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8e0be71e86ac6e45f13",
+    "url": "/static/css/main.113c32c3.chunk.css"
+  },
+  {
+    "revision": "5d824768570a3e039116",
+    "url": "/static/js/2.b8ad0e94.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b8ad0e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e0be71e86ac6e45f13",
+    "url": "/static/js/main.acf8adb7.chunk.js"
+  },
+  {
+    "revision": "6e837f648a4b9ad8a5ee",
+    "url": "/static/js/runtime-main.0d781a48.js"
+  }
+]);
